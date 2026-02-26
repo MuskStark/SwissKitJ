7661fb6fd28031644f610c3672204f1c88389ce7
MuskStart <gitmain@outlook.sg>
2026-02-26 18:13:31 +0800

    :art: Use UI designer to improve kit ui

---
+++ src/main/java/fan/summer/kitpage/email/EmailKitPage.java
@@ -0,0 +1,20 @@
+package fan.summer.kitpage.email;
+
+import javax.swing.*;
+
+/**
+ * 类的详细说明
+ *
+ * @author summer
+ * @version 1.00
+ * @Date 2026/2/26
+ */
+public class EmailKitPage {
+    private JPanel emailPanel;
+    private JTextField subject;
+    private JLabel emailTitle;
+    private JTextField toText;
+    private JLabel to;
+    private JTextArea textArea1;
+    private JButton sentButton;
+}
