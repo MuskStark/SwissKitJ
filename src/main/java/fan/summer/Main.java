cd75e0ec286f4ae1af295b817dadd859c5cc063d
MuskStart <gitmain@outlook.sg>
2026-03-03 16:55:09 +0800

    ✨ Feat: Add splash screen and database initialization on startup

--- src/main/java/fan/summer/Main.java
+++ src/main/java/fan/summer/Main.java
@@ -1,5 +1,7 @@
 package fan.summer;
 
+import fan.summer.database.DatabaseInit;
+import fan.summer.ui.StartLoadingPage;
 import fan.summer.ui.home.HomePage;
 
 import javax.swing.*;
@@ -7,8 +9,36 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Todo create program database
-        SwingUtilities.invokeLater(() -> new HomePage().init());
-    }
+        SwingUtilities.invokeLater(() -> {
+            JWindow splash = new StartLoadingPage().getWindow();
+            splash.setVisible(true);
 
+            new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() {
+                    try {
+                        DatabaseInit.init();
+                    } catch (Exception e) {
+                        publish((Void) null);
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void done() {
+                    splash.dispose();
+                    try {
+                        get();
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(null,
+                            "Database initialization failed: " + e.getCause().getMessage(),
+                            "Error", JOptionPane.ERROR_MESSAGE);
+                        System.exit(1);
+                        return;
+                    }
+                    new HomePage().init();
+                }
+            }.execute();
+        });
+    }
 }
